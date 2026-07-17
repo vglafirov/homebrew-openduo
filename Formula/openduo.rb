59354613eb2bfce680a930f25b1f68ab35e9adbd
OpenDuo CI <ci@gitlab.com>
2026-07-17 07:14:54 +0000

    formula: update to v1.18.3 (sha256: 11d35c104d5b...)

--- Formula/openduo.rb
+++ Formula/openduo.rb
@@ -2,8 +2,8 @@ class Openduo < Formula
   desc "GitLab-hardened wrapper for OpenCode"
   homepage "https://gitlab.com/vglafirov/openduo"
   # URL and SHA256 are updated automatically by script/release.sh
-  url "https://gitlab.com/vglafirov/openduo/-/archive/v1.18.2/openduo-v1.18.2.tar.gz"
-  sha256 "499c32de3b982e65e5ccc8c33a8d194707598484ebb11730c0b2b77e96503bf5"
+  url "https://gitlab.com/vglafirov/openduo/-/archive/v1.18.3/openduo-v1.18.3.tar.gz"
+  sha256 "11d35c104d5bc1c6f8ac732dc81e6a4b6ed4461c068e70780c4e8cfcfdeae4ea"
   license "MIT"
 
   depends_on "node"
